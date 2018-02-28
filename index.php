053242cd641b8935e46c4c29d307406dbd5a0fd8
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-28 15:07:20 -0700

    new repository to work on capstone homepage

--- index.php
+++ index.php
@@ -2,8 +2,40 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>Basic HTML Page</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+				integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
+
+		<div class="card-deck">
+			<div class="card">
+				<div class="card-body">
+					<h4 class="card-title">Card title</h4>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-body">
+					<h4 class="card-title">Card title</h4>
+					<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-body">
+					<h4 class="card-title">Card title</h4>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+				</div>
+			</div>
+		</div>
+
+	</body>
+
+
 
 </html>
